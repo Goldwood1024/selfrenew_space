d4f8aba1fac2fda7329209b6f41ea98c34c8e0f4
Goldwood1024 <2862379252@qq.com>
2022-10-25 21:36:02 +0800

    timer

--- lib/page/mobile/action/focus/timer.dart
+++ lib/page/mobile/action/focus/timer.dart
@@ -1,6 +1,7 @@
+import 'package:action_slider/action_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:selfrenew_space/export/selfrenew_flutter.dart';
-import 'package:sleek_circular_slider/sleek_circular_slider.dart';
+import 'package:slide_countdown/slide_countdown.dart';
 
 class FocusTimer extends StatefulWidget {
   const FocusTimer({
@@ -35,39 +36,76 @@ class _FocusTimerState extends State<FocusTimer>
         centerTitle: true,
         title: const Icon(
           CupertinoIcons.loop,
-          size: 20,
-        ),
-        leading: IconButton(
-          onPressed: () {
-            SmartDialog.dismiss();
-          },
-          icon: const Icon(
-            CupertinoIcons.multiply_circle,
-          ),
+          size: 30,
         ),
         actions: [],
       ),
       body: Padding(
         padding: SPHelper.pagePaddingHorizontal,
-        child: ListView(
+        child: Stack(
+          alignment: Alignment.center,
           children: [
-            Align(
-              child: Text(
-                '同意方法',
-                style: TextStyle(
-                  fontSize: SPHelper.sp(SPHelper.fontSp18),
-                  fontWeight: FontWeight.w500,
+            Positioned(
+              top: 0,
+              child: Align(
+                child: Text(
+                  '同意方法',
+                  style: TextStyle(
+                    fontSize: SPHelper.sp(SPHelper.fontSp18),
+                    fontWeight: FontWeight.w500,
+                    color: Theme.of(context).textTheme.displayLarge?.color,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: MediaQuery.of(context).size.height / 5,
+              child: SlideCountdown(
+                separatorType: SeparatorType.symbol,
+                slideDirection: SlideDirection.up,
+                decoration: const BoxDecoration(),
+                duration: const Duration(minutes: 25),
+                textStyle: TextStyle(
+                  fontSize: 56,
                   color: Theme.of(context).textTheme.displayLarge?.color,
                 ),
               ),
             ),
-            SleekCircularSlider(
-              min: 0,
-              max: 1000,
-              initialValue: 426,
-              onChange: (double value) {},
-              onChangeStart: (double startValue) {},
-              onChangeEnd: (double endValue) {},
+            Positioned(
+              bottom: 140,
+              child: Align(
+                child: ActionSlider.standard(
+                  width: 200.0,
+                  height: 52,
+                  icon: const Icon(
+                    Icons.keyboard_arrow_right_rounded,
+                    size: 32,
+                    color: Colors.white,
+                  ),
+                  successIcon: const Icon(
+                    Icons.check_rounded,
+                    size: 32,
+                    color: Colors.white,
+                  ),
+                  boxShadow: [],
+                  backgroundColor: CupertinoColors.systemGrey4,
+                  child: const Text(
+                    '滑动放弃',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: CupertinoColors.systemGrey2,
+                      fontSize: 16,
+                    ),
+                  ),
+                  action: (controller) async {
+                    controller.loading();
+                    await Future.delayed(const Duration(milliseconds: 500));
+                    controller.success();
+                    await Future.delayed(const Duration(seconds: 2));
+                    SmartDialog.dismiss();
+                  },
+                ),
+              ),
             )
           ],
         ),
