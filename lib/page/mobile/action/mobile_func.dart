feccb6b0b5f4cb5f983b86813683d1f52ecc1faa
wangwei <2862379252@qq.com>
2022-10-26 14:02:53 +0800

    timer

--- lib/page/mobile/action/mobile_func.dart
+++ lib/page/mobile/action/mobile_func.dart
@@ -14,47 +14,48 @@ class _MobileFuncState extends State<MobileFunc> {
   @override
   Widget build(BuildContext context) {
     return ScaffoldGradientBackground(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          title: const AppBarText(
-            title: '功能模块',
-          ),
-          actions: [
-            ActionBtn(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              title: '确定',
-            )
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const AppBarText(
+          title: '功能模块',
+        ),
+        actions: [
+          ActionBtn(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            title: '确定',
+          )
+        ],
+      ),
+      body: Padding(
+        padding: SPHelper.pagePaddingHorizontal,
+        child: ListView(
+          children: [
+            SPHelper.getDefaultHeightBox(),
+            SimpleTile(
+              title: '日常习惯',
+              topRadius: true,
+              onPressed: () {},
+              showArrow: false,
+              trailing: FlutterSwitch(
+                value: true,
+                onToggle: (bool value) {},
+              ),
+            ),
+            SimpleTile(
+              title: '专注',
+              bottomRadius: true,
+              onPressed: () {},
+              showArrow: false,
+              trailing: FlutterSwitch(
+                value: false,
+                onToggle: (bool value) {},
+              ),
+            ),
           ],
         ),
-        body: Padding(
-          padding: SPHelper.pagePaddingHorizontal,
-          child: ListView(
-            children: [
-              SPHelper.getDefaultHeightBox(),
-              SimpleTile(
-                title: '专注',
-                topRadius: true,
-                onPressed: () {},
-                showArrow: false,
-                trailing: FlutterSwitch(
-                  value: false,
-                  onToggle: (bool value) {},
-                ),
-              ),
-              SimpleTile(
-                title: '习惯',
-                bottomRadius: true,
-                onPressed: () {},
-                showArrow: false,
-                trailing: FlutterSwitch(
-                  value: true,
-                  onToggle: (bool value) {},
-                ),
-              ),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
