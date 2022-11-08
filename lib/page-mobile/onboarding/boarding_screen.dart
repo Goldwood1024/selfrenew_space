d1f97a9543b521979f590a1ae9fb083195c75204
wangwei <2862379252@qq.com>
2022-11-08 11:36:10 +0800

    onboarding

--- lib/page-mobile/onboarding/boarding_screen.dart
+++ lib/page-mobile/onboarding/boarding_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:introduction_screen/introduction_screen.dart';
+import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
 import 'package:selfrenew_space/selfrenew_flutter.dart';
 
 class BoardingScreen extends StatefulWidget {
@@ -12,6 +12,7 @@ class BoardingScreen extends StatefulWidget {
 
 class _BoardingScreenState extends State<BoardingScreen> {
   late bool firstOpenApp;
+  final double width = 400;
 
   @override
   void initState() {
@@ -26,119 +27,232 @@ class _BoardingScreenState extends State<BoardingScreen> {
     );
   }
 
+  final Color kDarkBlueColor = const Color(0xFF053149);
+
   @override
   Widget build(BuildContext context) {
+    // return firstOpenApp
+    //     ? IntroductionScreen(
+    //         globalBackgroundColor: Colors.white,
+    //         isProgressTap: false,
+    //         isBottomSafeArea: true,
+    //         isTopSafeArea: true,
+    //         next: Icon(
+    //           CupertinoIcons.forward,
+    //           color: Theme.of(context).primaryColor,
+    //         ),
+    //         done: Text(
+    //           '退出',
+    //           style: TextStyle(
+    //             fontSize: 18,
+    //             fontWeight: FontWeight.w600,
+    //             color: Theme.of(context).textTheme.labelSmall!.color,
+    //           ),
+    //         ),
+    //         onDone: () {
+    //           // TODO 写入文件
+    //           Routers.go(Routers.mobileHome);
+    //         },
+    //         showSkipButton: false,
+    //         skip: const Text(
+    //           '跳过',
+    //           style: TextStyle(
+    //             fontSize: 18,
+    //             fontWeight: FontWeight.w600,
+    //             color: CupertinoColors.secondaryLabel,
+    //           ),
+    //         ),
+    //         controlsMargin: const EdgeInsets.fromLTRB(0, 0, 0, 30),
+    //         pages: [
+    //           PageViewModel(
+    //             footer: Container(),
+    //             title: "Fractional shares",
+    //             body:
+    //                 "Instead of having to buy an entire share, invest any amount you want.",
+    //             image: _buildImage('img1.jpg'),
+    //             decoration: PageDecoration(
+    //               footerPadding: EdgeInsets.zero,
+    //               bodyAlignment: Alignment.center,
+    //               imagePadding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+    //               titleTextStyle: TextStyle(
+    //                 fontSize: 20.0,
+    //                 fontWeight: FontWeight.bold,
+    //                 color: Theme.of(context).textTheme.labelSmall!.color!,
+    //               ),
+    //               bodyTextStyle: TextStyle(
+    //                 fontSize: 18.0,
+    //                 fontWeight: FontWeight.normal,
+    //                 color: Theme.of(context)
+    //                     .textTheme
+    //                     .labelSmall!
+    //                     .color!
+    //                     .withOpacity(.8),
+    //               ),
+    //             ),
+    //           ),
+    //           PageViewModel(
+    //             title: "Fractional shares",
+    //             body:
+    //                 "Instead of having to buy an entire share, invest any amount you want.",
+    //             image: _buildImage('img2.jpg'),
+    //             decoration: PageDecoration(
+    //               footerPadding: EdgeInsets.zero,
+    //               bodyAlignment: Alignment.center,
+    //               imagePadding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+    //               titleTextStyle: TextStyle(
+    //                 fontSize: 20.0,
+    //                 fontWeight: FontWeight.bold,
+    //                 color: Theme.of(context).textTheme.labelSmall!.color!,
+    //               ),
+    //               bodyTextStyle: TextStyle(
+    //                 fontSize: 18.0,
+    //                 fontWeight: FontWeight.normal,
+    //                 color: Theme.of(context)
+    //                     .textTheme
+    //                     .labelSmall!
+    //                     .color!
+    //                     .withOpacity(.8),
+    //               ),
+    //             ),
+    //           ),
+    //           PageViewModel(
+    //             title: "Fractional shares",
+    //             body:
+    //                 "Instead of having to buy an entire share, invest any amount you want.",
+    //             image: _buildImage('img3.jpg'),
+    //             decoration: PageDecoration(
+    //               footerPadding: EdgeInsets.zero,
+    //               bodyAlignment: Alignment.center,
+    //               imagePadding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+    //               titleTextStyle: TextStyle(
+    //                 fontSize: 20.0,
+    //                 fontWeight: FontWeight.bold,
+    //                 color: Theme.of(context).textTheme.labelSmall!.color!,
+    //               ),
+    //               bodyTextStyle: TextStyle(
+    //                 fontSize: 18.0,
+    //                 fontWeight: FontWeight.normal,
+    //                 color: Theme.of(context)
+    //                     .textTheme
+    //                     .labelSmall!
+    //                     .color!
+    //                     .withOpacity(.8),
+    //               ),
+    //             ),
+    //           ),
+    //         ],
+    //       )
+    //     : const MobileHome();
+
     return firstOpenApp
-        ? IntroductionScreen(
-            globalBackgroundColor: Colors.white,
-            isProgressTap: false,
-            isBottomSafeArea: true,
-            isTopSafeArea: true,
-            next: Icon(
-              CupertinoIcons.forward,
-              color: Theme.of(context).primaryColor,
-            ),
-            done: Text(
-              '退出',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w600,
-                color: Theme.of(context).textTheme.labelSmall!.color,
-              ),
-            ),
-            onDone: () {
+        ? OnBoardingSlider(
+            finishButtonText: '开始专注',
+            onFinish: () {
               // TODO 写入文件
               Routers.go(Routers.mobileHome);
             },
-            showSkipButton: false,
-            skip: const Text(
+            finishButtonColor: Theme.of(context).primaryColor,
+            skipTextButton: Text(
               '跳过',
               style: TextStyle(
-                fontSize: 18,
+                fontSize: 16,
+                color: kDarkBlueColor,
                 fontWeight: FontWeight.w600,
-                color: CupertinoColors.secondaryLabel,
               ),
             ),
-            controlsMargin: const EdgeInsets.fromLTRB(0, 0, 0, 30),
-            pages: [
-              PageViewModel(
-                footer: Container(),
-                title: "Fractional shares",
-                body:
-                    "Instead of having to buy an entire share, invest any amount you want.",
-                image: _buildImage('img1.jpg'),
-                decoration: PageDecoration(
-                  footerPadding: EdgeInsets.zero,
-                  bodyAlignment: Alignment.center,
-                  imagePadding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
-                  titleTextStyle: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context).textTheme.labelSmall!.color!,
-                  ),
-                  bodyTextStyle: TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.normal,
-                    color: Theme.of(context)
-                        .textTheme
-                        .labelSmall!
-                        .color!
-                        .withOpacity(.8),
-                  ),
+            controllerColor: Theme.of(context).primaryColor,
+            totalPage: 3,
+            headerBackgroundColor: Colors.white,
+            pageBackgroundColor: Colors.white,
+            imageHorizontalOffset:
+                MediaQuery.of(context).size.width / 2 - width / 2,
+            background: [
+              Align(
+                child: Image.asset(
+                  'assets/splash/img1.jpg',
+                  width: width,
+                  height: width,
                 ),
               ),
-              PageViewModel(
-                title: "Fractional shares",
-                body:
-                    "Instead of having to buy an entire share, invest any amount you want.",
-                image: _buildImage('img2.jpg'),
-                decoration: PageDecoration(
-                  footerPadding: EdgeInsets.zero,
-                  bodyAlignment: Alignment.center,
-                  imagePadding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
-                  titleTextStyle: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context).textTheme.labelSmall!.color!,
-                  ),
-                  bodyTextStyle: TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.normal,
-                    color: Theme.of(context)
-                        .textTheme
-                        .labelSmall!
-                        .color!
-                        .withOpacity(.8),
-                  ),
-                ),
+              Image.asset(
+                'assets/splash/img2.jpg',
+                height: width,
+                width: width,
               ),
-              PageViewModel(
-                title: "Fractional shares",
-                body:
-                    "Instead of having to buy an entire share, invest any amount you want.",
-                image: _buildImage('img3.jpg'),
-                decoration: PageDecoration(
-                  footerPadding: EdgeInsets.zero,
-                  bodyAlignment: Alignment.center,
-                  imagePadding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
-                  titleTextStyle: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context).textTheme.labelSmall!.color!,
-                  ),
-                  bodyTextStyle: TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.normal,
-                    color: Theme.of(context)
-                        .textTheme
-                        .labelSmall!
-                        .color!
-                        .withOpacity(.8),
-                  ),
-                ),
+              Image.asset(
+                'assets/splash/img3.jpg',
+                height: width,
+                width: width,
+              ),
+            ],
+            speed: 1,
+            pageBodies: const [
+              BoardingBodyPage(
+                title: 'On your way...',
+                bodyText:
+                    'to find the perfect looking Onboarding for your app?',
+              ),
+              BoardingBodyPage(
+                title: 'You’ve reached your destination.',
+                bodyText: 'Sliding with animation',
+              ),
+              BoardingBodyPage(
+                title: 'Start now!',
+                bodyText:
+                    'Where everything is possible and customize your onboarding.',
               ),
             ],
           )
         : const MobileHome();
+  }
+}
+
+class BoardingBodyPage extends StatelessWidget {
+  final String title;
+  final String bodyText;
+
+  const BoardingBodyPage({
+    super.key,
+    required this.title,
+    required this.bodyText,
+  });
+
+  final Color kDarkBlueColor = const Color(0xFF053149);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          const SizedBox(
+            height: 480,
+          ),
+          Text(
+            title,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: kDarkBlueColor,
+              fontSize: 24.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            bodyText,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Colors.black26,
+              fontSize: 18.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import flutter_local_notifications
+import package_info_plus_macos
 import path_provider_macos
 import screen_retriever
 import share_plus
@@ -16,6 +17,7 @@ import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
